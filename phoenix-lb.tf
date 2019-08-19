4f71a96e375c70d4de99fd0d69a415b557c90af6
212601004 <michael.sanderson@ge.com>
2019-08-19 17:19:52 -0400

    Updates

--- phoenix-lb.tf
+++ phoenix-lb.tf
@@ -8,33 +8,43 @@
 //}
 
 # Create port for static IP for LB
-//resource "openstack_networking_port_v2" "static_ip_port" {
-//  name = "static_ip_port"
-//  network_id = "2d4168b2-0c5d-450a-85cc-bcaf8bc6d2b4"
-//  admin_state_up = true
-//  fixed_ip {
-//    ip_address = "10.153.16.50",
-//    subnet_id = "bd6e5922-b179-4f18-b499-9e76efa290ae"
-//  }
+resource "openstack_networking_port_v2" "static_ip_port" {
+  name = "static_ip_port"
+  network_id = "2d4168b2-0c5d-450a-85cc-bcaf8bc6d2b4"
+  admin_state_up = true
+  fixed_ip {
+    ip_address = "10.153.16.50",
+    subnet_id = "bd6e5922-b179-4f18-b499-9e76efa290ae"
+  }
 //  binding = {
 //    host_id = "${openstack_lb_loadbalancer_v2.phoenix-lb.id}"
 //  }
+}
+
+//resource "openstack_lb_vip_v1" "phoenix-lb_vip" {
+//  name      = "phoenix-lb_vip"
+//  subnet_id = "f9488a92-288b-4f44-ad1e-ceb5590f1202"
+//  protocol  = "HTTP"
+//  port      = 80
+//  pool_id   = "67890"
 //}
 
-resource "openstack_lb_vip_v1" "phoenix-lb_vip" {
-  name      = "phoenix-lb_vip"
-  subnet_id = "f9488a92-288b-4f44-ad1e-ceb5590f1202"
-  protocol  = "HTTP"
-  port      = 80
-  pool_id   = "67890"
+resource "openstack_networking_floatingip_v2" "phoenix-lb_fip" {
+  pool = "GEIX-ATL1-CRP1-Internal"
+  port_id = "${openstack_networking_port_v2.static_ip_port.id}"
+}
+
+resource "openstack_networking_floatingip_associate_v2" "attach_phoenix-lb_fip" {
+  floating_ip = "${openstack_lb_loadbalancer_v2.phoenix-lb.vip_address}"
+  port_id     = "${openstack_networking_floatingip_v2.phoenix-lb_fip.port_id}"
 }
 
 # Create Phoenix Loadbalancer
 resource "openstack_lb_loadbalancer_v2" "phoenix-lb" {
-  vip_subnet_id = "bd6e5922-b179-4f18-b499-9e76efa290ae"
+  vip_subnet_id = "f9488a92-288b-4f44-ad1e-ceb5590f1202"
   name = "phoenix-lb"
   region     = "US-EAST2"
-  #vip_address = "${openstack_networking_port_v2.static_ip_port.all_fixed_ips}"
+  #vip_subnet_id = "${openstack_networking_port_v2.static_ip_port.all_fixed_ips}"
   depends_on      = [
     "openstack_compute_instance_v2.phoenix-server",
   ]
